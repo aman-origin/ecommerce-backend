b1e348df0737f56c4bfc7743c2e1c867471d11bf
Amanda2455 <amandavlkrbhr@gmail.com>
2026-03-15 01:09:54 +0530

    Implemented User and Address entities with JPA mappings

---
+++ src/main/java/com/aman/ecommerce/user/entity/User.java
@@ -0,0 +1,34 @@
+package com.aman.ecommerce.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    private LocalDateTime createdAt;
+}
