c4d956ff9e0df54660e4dbf919879dec2a923706
Amanda2455 <amandavlkrbhr@gmail.com>
2026-03-15 02:56:59 +0530

    implement product entities : category, product, productImage

---
+++ src/main/java/com/aman/ecommerce/product/entity/Product.java
@@ -0,0 +1,33 @@
+package com.aman.ecommerce.product.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Integer stock;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
